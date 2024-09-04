2f4a485d85201c40850ed0810d0d5ea85ea53301
ericmargadonna <eric082659@gmail.com>
2024-09-04 17:37:47 -0400

    Began to create and integrate tests

---
+++ tests/test_auth.py
@@ -0,0 +1,5 @@
+import pytest
+from flask import g, session
+from ramApp.db import get_db
+
+def test_register
---
+++ tests/test_db.py
@@ -0,0 +1,28 @@
+import sqlite3 as sql
+
+import pytest
+from ramApp.db import get_db
+
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    with pytest.raises(sql.ProgrammingError) as e:
+        db.execute('SELECT 1')
+    
+    assert 'closed' in str(e.value)
+
+    def test_init_db_command(runner, monkeypatch):
+        class Recorder(object):
+            called = False
+
+        def fake_init_db():
+            Recorder.called = True
+
+        monkeypatch.setattr('ramApp.db.init_db', fake_init_db)
+        result = runner.invoke(args=['init-db'])
+        assert 'Initialized' in result.output
+        assert Recorder.called
+
+    
